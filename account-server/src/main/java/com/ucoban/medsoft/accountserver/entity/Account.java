7215ed00e96db0a9aee894acd9f8eae4718ba90e
Ümit Yasin Çoban <umityasincoban@gmail.com>
2024-04-03 05:55:40 +0300

    feat: added account and role entity

--- account-server/src/main/java/com/ucoban/medsoft/accountserver/entity/Account.java
+++ account-server/src/main/java/com/ucoban/medsoft/accountserver/entity/Account.java
@@ -2,23 +2,103 @@ package com.ucoban.medsoft.accountserver.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "accounts")
 public class Account extends BaseEntity {
-
     @Id
+    @Column(name = "account_id")
     private String id;
     @Column(nullable = false)
     private String firstName;
-
     @Column(nullable = false)
     private String lastName;
-
     @Column(nullable = false)
     private String email;
-
     @Column(nullable = false)
     private String phone;
+    @ManyToMany(cascade = CascadeType.ALL, targetEntity = Role.class)
+    @JoinTable(name = "account_roles", joinColumns = @JoinColumn(name = "account_id", nullable = false), inverseJoinColumns = @JoinColumn(name = "role_id", nullable = false))
+    private Set<Role> roles;
 
+    public Account(String id, String firstName, String lastName, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
 
+    public Account() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Account acc && acc.id.equals(this.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
---
+++ account-server/src/main/java/com/ucoban/medsoft/accountserver/entity/Role.java
@@ -0,0 +1,62 @@
+package com.ucoban.medsoft.accountserver.entity;
+
+import jakarta.persistence.*;
+
+
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private long id;
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+    @ManyToMany(mappedBy = "roles")
+    private Set<Account> accounts;
+
+    public Role(long id, ERole role) {
+        this.id = id;
+        this.role = role;
+    }
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof Role r && r.id == id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
